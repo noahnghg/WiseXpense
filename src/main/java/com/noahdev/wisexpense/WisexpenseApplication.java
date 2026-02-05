e7a4842c7f6216b7c772a08233b79d11a608b20b
Noah <noahnghgwork@gmail.com>
2026-02-05 03:26:07 -0700

    chore: initalize project

---
+++ src/main/java/com/noahdev/wisexpense/WisexpenseApplication.java
@@ -0,0 +1,13 @@
+package com.noahdev.wisexpense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WisexpenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WisexpenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noahdev/wisexpense/WisexpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noahdev.wisexpense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WisexpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
